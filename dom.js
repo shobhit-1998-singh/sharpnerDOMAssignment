a83aa775f969a89499afd890c433c4e5c4394289
shobhit-1998-singh <shobhit.1621150@gmail.com>
2023-10-03 13:15:38 +0530

    QuerySelector's

--- dom.js
+++ dom.js
@@ -28,3 +28,14 @@ var li = document.getElementsByTagName('li');
 for(var i=0; i<li.length; i++){
     li[i].style.fontWeight = 'bold';
 }
+// QUERYSELECTOR //
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'green';
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display= 'none';
+
+// Queryselectorall//
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i= 0; i<odd.length; i++){
+    odd[i].style.backgroundColor='green';
+}
